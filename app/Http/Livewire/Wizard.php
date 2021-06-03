cb50377c0e1ea66f22a75a389d8b061418380280
toninow <abenalcazarc@gmail.com>
2021-06-02 21:43:07 -0500

    view wizard livewire created

--- app/Http/Livewire/Wizard.php
+++ app/Http/Livewire/Wizard.php
@@ -1,13 +1,84 @@
 <?php
 
 namespace App\Http\Livewire;
-
 use Livewire\Component;
+use App\Models\Team;
 
 class Wizard extends Component
 {
+    public $currentStep = 1;
+    public $name, $price, $detail, $status = 1;
+    public $successMsg = '';
+
+    /**
+     * Write code on Method
+     */
     public function render()
     {
         return view('livewire.wizard');
+    }
+
+    /**
+     * Write code on Method
+     */
+    public function firstStepSubmit()
+    {
+        $validatedData = $this->validate([
+            'name' => 'required',
+            'price' => 'required|numeric',
+            'detail' => 'required',
+        ]);
+
+        $this->currentStep = 2;
+    }
+
+    /**
+     * Write code on Method
+     */
+    public function secondStepSubmit()
+    {
+        $validatedData = $this->validate([
+            'status' => 'required',
+        ]);
+
+        $this->currentStep = 3;
+    }
+
+    /**
+     * Write code on Method
+     */
+    public function submitForm()
+    {
+        Team::create([
+            'name' => $this->name,
+            'price' => $this->price,
+            'detail' => $this->detail,
+            'status' => $this->status,
+        ]);
+
+        $this->successMsg = 'Team successfully created.';
+
+        $this->clearForm();
+
+        $this->currentStep = 1;
+    }
+
+    /**
+     * Write code on Method
+     */
+    public function back($step)
+    {
+        $this->currentStep = $step;
+    }
+
+    /**
+     * Write code on Method
+     */
+    public function clearForm()
+    {
+        $this->name = '';
+        $this->price = '';
+        $this->detail = '';
+        $this->status = 1;
     }
 }
--- resources/views/livewire/wizard.blade.php
+++ resources/views/livewire/wizard.blade.php
@@ -1,3 +1,91 @@
 <div>
-    {{-- Care about people's approval and you will be their prisoner. --}}
+    @if(!empty($successMsg))
+    <div class="alert alert-success">
+        {{ $successMsg }}
+    </div>
+    @endif
+    <div class="stepwizard">
+        <div class="stepwizard-row setup-panel">
+            <div class="multi-wizard-step">
+                <a href="#step-1" type="button"
+                    class="btn {{ $currentStep != 1 ? 'btn-default' : 'btn-primary' }}">1</a>
+                <p>Step 1</p>
+            </div>
+            <div class="multi-wizard-step">
+                <a href="#step-2" type="button"
+                    class="btn {{ $currentStep != 2 ? 'btn-default' : 'btn-primary' }}">2</a>
+                <p>Step 2</p>
+            </div>
+            <div class="multi-wizard-step">
+                <a href="#step-3" type="button"
+                    class="btn {{ $currentStep != 3 ? 'btn-default' : 'btn-primary' }}"
+                    disabled="disabled">3</a>
+                <p>Step 3</p>
+            </div>
+        </div>
+    </div>
+    <div class="row setup-content {{ $currentStep != 1 ? 'display-none' : '' }}" id="step-1">
+        <div class="col-md-12">
+            <h3> Step 1</h3>
+            <div class="form-group">
+                <label for="title">Team Name:</label>
+                <input type="text" wire:model="name" class="form-control" id="taskTitle">
+                @error('name') <span class="error">{{ $message }}</span> @enderror
+            </div>
+            <div class="form-group">
+                <label for="description">Team Price:</label>
+                <input type="text" wire:model="price" class="form-control" id="teamPrice" />
+                @error('price') <span class="error">{{ $message }}</span> @enderror
+            </div>
+            <div class="form-group">
+                <label for="detail">Team Details:</label>
+                <textarea type="text" wire:model="detail" class="form-control"
+                    id="taskDetail">{{{ $detail ?? '' }}}</textarea>
+                @error('detail') <span class="error">{{ $message }}</span> @enderror
+            </div>
+            <button class="btn btn-primary nextBtn btn-lg pull-right" wire:click="firstStepSubmit"
+                type="button">Next</button>
+        </div>
+    </div>
+    <div class="row setup-content {{ $currentStep != 2 ? 'display-none' : '' }}" id="step-2">
+        <div class="col-md-12">
+            <h3> Step 2</h3>
+            <div class="form-group">
+                <label for="description">Team Status</label><br />
+                <label class="radio-inline"><input type="radio" wire:model="status" value="1"
+                        {{{ $status == '1' ? "checked" : "" }}}> Active</label>
+                <label class="radio-inline"><input type="radio" wire:model="status" value="0"
+                        {{{ $status == '0' ? "checked" : "" }}}> DeActive</label>
+                @error('status') <span class="error">{{ $message }}</span> @enderror
+            </div>
+            <button class="btn btn-primary nextBtn btn-lg pull-right" type="button"
+                wire:click="secondStepSubmit">Next</button>
+            <button class="btn btn-danger nextBtn btn-lg pull-right" type="button" wire:click="back(1)">Back</button>
+        </div>
+    </div>
+    <div class="row setup-content {{ $currentStep != 3 ? 'display-none' : '' }}" id="step-3">
+        <div class="col-md-12">
+            <h3> Step 3</h3>
+            <table class="table">
+                <tr>
+                    <td>Team Name:</td>
+                    <td><strong>{{$name}}</strong></td>
+                </tr>
+                <tr>
+                    <td>Team Price:</td>
+                    <td><strong>{{$price}}</strong></td>
+                </tr>
+                <tr>
+                    <td>Team status:</td>
+                    <td><strong>{{$status ? 'Active' : 'DeActive'}}</strong></td>
+                </tr>
+                <tr>
+                    <td>Team Detail:</td>
+                    <td><strong>{{$detail}}</strong></td>
+                </tr>
+            </table>
+            <button class="btn btn-success btn-lg pull-right" wire:click="submitForm" type="button">Finish!</button>
+            <button class="btn btn-danger nextBtn btn-lg pull-right" type="button" wire:click="back(2)">Back</button>
+        </div>
+    </div>
 </div>
